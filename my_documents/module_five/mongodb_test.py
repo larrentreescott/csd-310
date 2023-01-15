e18c4179977889702201c7a9a96f5547525ef83f
larrentreescott <57553790+larrentreescott@users.noreply.github.com>
2023-01-15 15:34:40 -0600

    Update mongodb_test.py

--- my_documents/module_five/mongodb_test.py
+++ my_documents/module_five/mongodb_test.py
@@ -1,3 +1,4 @@
+
 # Scott larrentree
 #Module 5.2
 
@@ -6,4 +7,8 @@ from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.4diuzc0.mongodb.net/?retryWrites=true&w=majority";
 client = MongoClient(url);
 db = client.pytech;
+
+print(" -- Pytech Collection List -- \n");
 print(db.list_collection_names());
+
+input('End of program, press any key to exit');
